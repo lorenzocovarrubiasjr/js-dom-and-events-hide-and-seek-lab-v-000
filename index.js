507f861459c52dd187297095b82f02d82ff84764
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-28 05:53:33 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+};
+
+function nestedTarget() {
+    return document.getElementById('nested').querySelector('div.target');
+};
+
+
+function increaseRankBy(n) {
+    const lis = document.querySelectorAll('ul.ranked-list');
+    for (let i = 0; i < lis.length; i++) {
+      lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    }
+    return lis;
+};
+
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelector('div').querySelector('div').querySelector('div').querySelector('div');
+};
